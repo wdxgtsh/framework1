8d785e25f7ab76d73ae22a306b37c1e67f5903e8
zhaolei <zhaolei@allinmd.cn>
2016-01-19 15:54:01 +0800

    add framework

---
+++ Framework1/Framework1/Framework1.h
@@ -0,0 +1,13 @@
+//
+//  Framework1.h
+//  Framework1
+//
+//  Created by zhaolei on 16/1/19.
+//  Copyright © 2016年 zhaolei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Framework1 : NSObject
+
+@end
---
+++ Framework1/Framework1/Framework1.m
@@ -0,0 +1,13 @@
+//
+//  Framework1.m
+//  Framework1
+//
+//  Created by zhaolei on 16/1/19.
+//  Copyright © 2016年 zhaolei. All rights reserved.
+//
+
+#import "Framework1.h"
+
+@implementation Framework1
+
+@end
